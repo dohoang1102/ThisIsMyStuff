5926ca3d4b66fffa36d105677a2f150620c8fbf1
Eifion Bedford <eifion@asciicasts.com>
2010-09-06 10:57:13 +0100

    Renamed mis-spelt MusicRootViewControl(l)er.

---
+++ Classes/MusicRootViewController.h
@@ -0,0 +1,16 @@
+//
+//  MusicRootViewController.h
+//  ThisIsMyStuff
+//
+//  Created by Eifion Bedford on 06/09/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MusicRootViewController : UITableViewController {
+
+}
+
+@end
